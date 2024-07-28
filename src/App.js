71cc83169659c48fc7aafce41d50ca23a222676d
iis istiqomah <iisistiqomah20@gmail.com>
2024-07-28 15:07:36 +0700

    State Class Component: Contoh 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import UserClass from "./components/UserClass";
-import UserFunctional from "./components/UserFunctional";
+import Contoh1 from './components/Contoh1';
 import './App.css';
 
 function App() {
-  const name = "Ngoding Channel";
-  const email = "ngoding@gmail.com";
-  const pengikut = 10000;
-
   return (
     <div className="App">
       <header className="App-header">
-        <UserFunctional channel="Coding Channel" email="coding@gmail.com" />
-        <UserClass
-          name="ReactJS Channel"
-          email="reactjs@gmail.com"
-          subscriber={5000}
-        />
-        <UserClass name={name} email={email} subscriber={pengikut} />
+        <Contoh1 />
       </header>
     </div>
   );
---
+++ src/components/Contoh1.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class Home1 extends Component {
+  // cara 1 : membuat initial state
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     navigation: "initial",
+  //     counter: 0,
+  //   };
+  // }
+
+  // cara 2 : membuat initial state
+  state = {
+    navigation: "initial",
+    counter: 0,
+  };
+
+  addCounter = () => {
+    // cara 1 : mengubah state
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
+  render() {
+    console.log("Render Component");
+    const { counter } = this.state;
+
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>{counter}</p>
+        <button onClick={this.addCounter}>Add Counter</button>
+      </div>
+    );
+  }
+}
+
+export default Home1;
